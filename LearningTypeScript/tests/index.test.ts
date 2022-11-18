67ab5d6dd6a0e9b6bf8d9d4ca08fc5f3ca440756
Sten Johnsen <sten@thinea.no>
2022-11-18 12:59:47 +0100

    wip: add rangeError test

--- LearningTypeScript/tests/index.test.ts
+++ LearningTypeScript/tests/index.test.ts
@@ -7,4 +7,11 @@ describe('testing index file', () => {
   test('1 plus two shoul result in 3', () => {
     expect(add('1,2')).toBe(3)
   })
+  test('2 plus three shoul result in 5', () => {
+    expect(add('2,3')).toBe(5)
+  })
+
+  test('negative number should result in error message', () => {
+    expect(add('1,-1')).toThrow(new RangeError('Negatives are not allowed'))
+  })
 });
