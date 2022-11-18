c06d6028391c683e89eb45279571ff74feab7b8f
Sten Johnsen <sten@thinea.no>
2022-11-18 12:39:57 +0100

    wip: add test for add 1 and 2

--- LearningTypeScript/tests/index.test.ts
+++ LearningTypeScript/tests/index.test.ts
@@ -4,4 +4,7 @@ describe('testing index file', () => {
   test('empty string should result in zero', () => {
     expect(add('')).toBe(0);
   });
+  test('1 plus two shoul result in 3', () => {
+    expect(add('1,2')).toBe(3)
+  })
 });
